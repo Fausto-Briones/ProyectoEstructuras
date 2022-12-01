42763f069eda303a0012d6dc7457572e5121fb3d
AxcelVillagran <107808778+AxcelVillagran@users.noreply.github.com>
2022-11-30 19:19:41 -0500

    Add files via upload

---
+++ ProyectoEDDPrimerParcial/src/main/java/Modelo/Resenia.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProyectoEDD;
+
+/**
+ *
+ * @author AVGla
+ */
+public class Resenia {
+    
+    String usuario;
+    String comentario;
+    int valoracion;
+    int anio;
+    
+    public Resenia(String usuario,String comentario,int valoracion,int anio){
+    this.usuario=usuario;
+    this.comentario=comentario;
+    this.valoracion=valoracion;
+    this.anio=anio;
+    }
+    
+    public String getUsuario(){
+    return usuario;
+    }
+    public String getComment(){
+    return comentario;
+    }
+    public int getValoracion(){
+    return valoracion;
+    }
+    public int getAnio(){
+    return anio;
+    }
+}
